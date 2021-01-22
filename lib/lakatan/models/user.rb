c134620759056be1731174790e6f3d9fd600c745
Leandro Segovia <ldlsegovia@gmail.com>
2021-01-22 15:21:13 -0300

    feat(models/user): define attributes

--- lib/lakatan/models/user.rb
+++ lib/lakatan/models/user.rb
@@ -1,4 +1,10 @@
 module Lakatan
   class User < Lakatan::BaseModel
+    declare_attribute(:name, :string)
+    declare_attribute(:email, :string)
+    declare_attribute(:created_at, :datetime)
+    declare_attribute(:updated_at, :datetime)
+    declare_attribute(:last_org, :integer)
+    declare_attribute(:team_ids)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,15 @@
 require "spec_helper"
 
 describe Lakatan::User do
-  it_behaves_like "find resource", 115
+  it_behaves_like "find resource", 115 do
+    it { expect(resource.id).to eq(resource_id) }
+    it { expect(resource.name).to eq("Andrés Matte") }
+    it { expect(resource.email).to eq("andres@platan.us") }
+    it { expect(resource.created_at).to be_a(DateTime) }
+    it { expect(resource.updated_at).to be_a(DateTime) }
+    it { expect(resource.last_org).to be_nil }
+    it { expect(resource.team_ids).to contain_exactly(96, 97, 103) }
+  end
+
   it_behaves_like "all resources"
 end
